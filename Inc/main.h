aa4914dcc43ebae1ed7383f6d9dd1cc6c6c2bcb7
Thibaut VERCUEIL <thibaut.vercueil@st.com>
2021-10-18 14:35:36 +0200

    Initial commit

---
+++ Inc/main.h
@@ -0,0 +1,119 @@
+/**
+  ******************************************************************************
+  * @file    BSP/CM7/Inc/main.h
+  * @author  MCD Application Team
+  * @brief   Header for main.c module for Cortex-M7.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdio.h"
+#include "string.h"
+#include "stm32h7xx_hal.h"
+#include "stm32h747i_discovery.h"
+#include "stm32h747i_discovery_lcd.h"
+#include "stm32h747i_discovery_conf.h"
+#include "stm32h747i_discovery_sd.h"
+#include "stm32h747i_discovery_ts.h"
+#include "stm32h747i_discovery_qspi.h"
+#include "stm32h747i_discovery_camera.h"
+#include "stm32h747i_discovery_sdram.h"
+#include "stm32_lcd.h"
+
+/* Exported types ------------------------------------------------------------*/
+typedef struct
+{
+  void   (*DemoFunc)(void);
+  uint8_t DemoName[50];
+  uint32_t DemoIndex;
+}BSP_DemoTypedef;
+
+
+#define SD_DMA_MODE      0U
+#define SD_IT_MODE       1U
+#define SD_POLLING_MODE  2U
+
+/* Exported variables --------------------------------------------------------*/
+extern __IO uint32_t SRAMTest;
+#ifndef USE_FULL_ASSERT
+extern uint32_t    ErrorCounter;
+#endif
+extern uint32_t JoyStickDemo ;
+extern __IO uint32_t ButtonState;
+extern __IO uint32_t JoystickStates;
+extern __IO uint32_t CameraTest;
+extern __IO uint32_t SdramTest;
+extern __IO uint32_t SdmmcTest;
+/* Exported constants --------------------------------------------------------*/
+/**
+  * @brief  SDRAM Write read buffer start address after CAM Frame buffer
+  * Assuming Camera frame buffer is of size 800x480 and format ARGB8888 (32 bits per pixel).
+  */
+#define SDRAM_WRITE_READ_ADDR_OFFSET ((uint32_t)0x0800)
+
+/* Defines for the Audio playing process */
+#define PAUSE_STATUS     ((uint32_t)0x00) /* Audio Player in Pause Status */
+#define RESUME_STATUS    ((uint32_t)0x01) /* Audio Player in Resume Status */
+#define IDLE_STATUS      ((uint32_t)0x02) /* Audio Player in Idle Status */
+
+
+#define LED_GREEN      LED1
+#define LED_ORANGE      LED2
+#define LED_RED      LED3
+#define LED_BLUE      LED4
+#define CAMERA_FRAME_TMP              0xD020D000
+/* SDRAM write address */
+#define SDRAM_WRITE_READ_ADDR         0xD0177000
+
+/* Exported macro ------------------------------------------------------------*/
+#define COUNT_OF_EXAMPLE(x)    (sizeof(x)/sizeof(BSP_DemoTypedef))
+
+#ifdef USE_FULL_ASSERT
+/* Assert activated */
+#define ASSERT(__condition__)                do { if(__condition__) \
+                                                   {  assert_failed(__FILE__, __LINE__); \
+                                                      while(1);  \
+                                                    } \
+                                              }while(0)
+#else
+/* Assert not activated : macro has no effect */
+#define ASSERT(__condition__)                  do { if(__condition__) \
+                                                   {  ErrorCounter++; \
+                                                    } \
+                                              }while(0)
+#endif /* USE_FULL_ASSERT */
+
+/* Exported functions ------------------------------------------------------- */
+void Touchscreen_demo1 (void);
+void Touchscreen_demo2 (void);
+void LCD_demo (void);
+void Camera_demo (void);
+void Joystick_demo (void);
+void SD_DMA_demo(void);
+void SD_IT_demo(void);
+void SD_POLLING_demo(void);
+void Error_Handler(void);
+void SDRAM_demo(void);
+void SDRAM_DMA_demo (void);
+void QSPI_demo (void);
+uint8_t CheckForUserInput(void);
+uint8_t TouchScreen_GetTouchPosition(void);
+void Touchscreen_DrawBackground_Circles(uint8_t state);
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Src/stm32h7xx_it.c
@@ -0,0 +1,248 @@
+/**
+  ******************************************************************************
+  * @file    BSP/CM7/Src/stm32h7xx_it.c
+  * @author  MCD Application Team
+  * @brief   Main Interrupt Service Routines for Cortex-M7.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32h7xx_it.h"
+
+/** @addtogroup STM32H7xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup DMAMUX_SYNC
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M7 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+/******************************************************************************/
+/*                 STM32H7xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32h7xx.s).                                               */
+/******************************************************************************/
+
+/**
+* @brief  This function handles External line 2 interrupt request.
+* @param  None
+* @retval None
+*/
+void EXTI2_IRQHandler(void)
+{
+   BSP_JOY_IRQHandler(JOY1, JOY_SEL);
+}
+
+/**
+* @brief  This function handles External line 3 interrupt request.
+* @param  None
+* @retval None
+*/
+void EXTI3_IRQHandler(void)
+{
+   BSP_JOY_IRQHandler(JOY1, JOY_DOWN);
+}
+
+/**
+* @brief  This function handles External line 4 interrupt request.
+* @param  None
+* @retval None
+*/
+void EXTI4_IRQHandler(void)
+{
+   BSP_JOY_IRQHandler(JOY1, JOY_LEFT);
+}
+
+/**
+* @brief  This function handles External lines 9 to 5 interrupt request.
+* @param  None
+* @retval None
+*/
+void EXTI9_5_IRQHandler(void)
+{
+  BSP_JOY_IRQHandler(JOY1, JOY_RIGHT);
+  BSP_JOY_IRQHandler(JOY1, JOY_UP);
+}
+
+/**
+  * @brief  This function handles External lines 15 to 10 interrupt request.
+  * @param  None
+  * @retval None
+  */
+void EXTI15_10_IRQHandler(void)
+{
+  BSP_PB_IRQHandler(BUTTON_WAKEUP);
+}
+
+
+/**
+  * @brief  Handles MDMA transfer interrupt request.
+  * @retval None
+  */
+void MDMA_IRQHandler(void)
+{
+    BSP_SDRAM_IRQHandler(0);
+}
+
+/**
+  * @brief  This function handles SD interrupt request.
+  * @param  None
+  * @retval None
+  */
+void SDMMC1_IRQHandler(void)
+{
+  BSP_SD_IRQHandler(0);
+}
+
+/**
+  * @brief  DCMI interrupt handler.
+  * @param  None
+  * @retval None
+  */
+void DCMI_IRQHandler(void)
+{
+   BSP_CAMERA_IRQHandler(0);
+}
+
+/**
+  * @brief  DMA interrupt handler.
+  * @param  None
+  * @retval None
+  */
+void DMA2_Stream3_IRQHandler(void)
+{
+  BSP_CAMERA_DMA_IRQHandler(0);
+}
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
