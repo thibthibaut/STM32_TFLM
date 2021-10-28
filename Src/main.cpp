4d268eea7b7b24430abbbb50f7d99dcb71dcd5e0
Thibaut VERCUEIL <thibaut.vercueil@st.com>
2021-10-28 11:40:57 +0200

    Cleanup

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,60 +3,62 @@
 uint8_t DemoIndex = 0;
 __IO uint8_t NbLoop = 1;
 
-#ifndef USE_FULL_ASSERT
-uint32_t    ErrorCounter = 0;
-#endif
+#define CAMERA_RES_INDEX_MIN CAMERA_R160x120
+#define CAMERA_RES_INDEX_MAX CAMERA_R640x480
+
+extern __IO uint32_t ButtonState;
+static __IO uint32_t JoyState = JOY_NONE;
+
+static volatile uint32_t CameraFrameBufferInitComplete = 0;
+static volatile uint32_t CameraFrameBufferInitError = 0;
+
+uint32_t CameraResolution[4] = {CAMERA_R160x120, CAMERA_R320x240,
+                                CAMERA_R480x272, CAMERA_R640x480};
+uint32_t CameraResX[5] = {160, 320, 480, 640, 800};
+uint32_t CameraResY[5] = {120, 240, 272, 480, 480};
+uint32_t xsize, ysize;
+uint32_t index_resolution = 1;
+
+volatile uint32_t Camera_AllowDma2dCopyCamFrmBuffToLcdFrmBuff = 0;
+static void LCD_LL_Convert_RGB565ToARGB8888(void *pSrc, void *pDst,
+                                            uint16_t xsize, uint16_t ysize);
+
+/* Wave Player Pause/Resume Status. Defined as external in waveplayer.c file */
+__IO uint32_t PauseResumeStatus = IDLE_STATUS;
 
 /* Counter for Sel Joystick pressed*/
 __IO uint32_t PressCount = 0;
-__IO uint32_t ButtonState=0;
+__IO uint32_t ButtonState = 0;
 uint8_t toggle_led = 0;
+__IO uint32_t CameraTest = 0;
 
-__IO uint32_t CameraTest=0;
-
+__IO uint8_t volume = 60;
+__IO uint8_t VolumeChange = 0;
 __IO uint32_t SRAMTest = 0;
-__IO uint32_t SdramTest=0;
-/* Private function prototypes -----------------------------------------------*/
+__IO uint32_t SdramTest = 0;
+
 static void SystemClock_Config(void);
 static void Display_DemoDescription(void);
 static void MPU_Config(void);
 static void CPU_CACHE_Enable(void);
 
-BSP_DemoTypedef  BSP_examples[]=
-{
-  {Camera_demo, "CAMERA", 0},
-};
-
-int main(void)
-{
-  /* System Init, System clock, voltage scaling and L1-Cache configuration are done by CPU1 (Cortex-M7)
-     in the meantime Domain D2 is put in STOP mode(Cortex-M4 in deep-sleep)
-  */
-
+/**
+ * @brief  Main program
+ * @param  None
+ * @retval None
+ */
+int main(void) {
   /* Configure the MPU attributes as Write Through */
   MPU_Config();
 
   /* Enable the CPU Cache */
- CPU_CACHE_Enable();
+  CPU_CACHE_Enable();
 
-  /* STM32H7xx HAL library initialization:
-       - Systick timer is configured by default as source of time base, but user
-         can eventually implement his proper time base source (a general purpose
-         timer for example or other time source), keeping in mind that Time base
-         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
-         handled in milliseconds basis.
-       - Set NVIC Group Priority to 4
-       - Low Level Initialization
-     */
+  /* STM32H7xx HAL library initialization: */
   HAL_Init();
 
   /* Configure the system clock to 400 MHz */
   SystemClock_Config();
-
-
-  /* When system initialization is finished, Cortex-M7 could wakeup (when needed) the Cortex-M4  by means of
-     HSEM notification or by any D2 wakeup source (SEV,EXTI..)   */
-
 
   /* Configure the Wakeup push-button in EXTI Mode */
   BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_EXTI);
@@ -65,44 +67,50 @@ int main(void)
   BSP_LED_Init(LED3);
   BSP_LED_Init(LED4);
 
-  /*##-1- Initialize the LCD #################################################*/
   /* Initialize the LCD */
   BSP_LCD_Init(0, LCD_ORIENTATION_LANDSCAPE);
   UTIL_LCD_SetFuncDriver(&LCD_Driver);
   UTIL_LCD_SetFont(&UTIL_LCD_DEFAULT_FONT);
-  Display_DemoDescription();
-  Camera_demo();
-  /* Wait For User inputs */
-  while (1)
-  {
+
+  UTIL_LCD_SetBackColor(UTIL_LCD_COLOR_BLACK);
+  UTIL_LCD_Clear(UTIL_LCD_COLOR_BLACK);
+
+  uint32_t camera_status = BSP_ERROR_NONE;
+
+  camera_status = BSP_CAMERA_Init(0, CAMERA_R320x240, CAMERA_PF_RGB565);
+
+  if (camera_status != BSP_ERROR_NONE) {
+    Error_Handler();
   }
 
+  HAL_Delay(200);
+
+  BSP_CAMERA_Start(0, (uint8_t *)CAMERA_FRAME_BUFFER, CAMERA_MODE_CONTINUOUS);
+
+  while (1) {
+  }
 }
 
 /**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
-  *            System Clock source            = PLL (HSE)
-  *            SYSCLK(Hz)                     = 400000000 (Cortex-M7 CPU Clock)
-  *            HCLK(Hz)                       = 200000000 (Cortex-M4 CPU, Bus matrix Clocks)
-  *            AHB Prescaler                  = 2
-  *            D1 APB3 Prescaler              = 2 (APB3 Clock  100MHz)
-  *            D2 APB1 Prescaler              = 2 (APB1 Clock  100MHz)
-  *            D2 APB2 Prescaler              = 2 (APB2 Clock  100MHz)
-  *            D3 APB4 Prescaler              = 2 (APB4 Clock  100MHz)
-  *            HSE Frequency(Hz)              = 25000000
-  *            PLL_M                          = 5
-  *            PLL_N                          = 160
-  *            PLL_P                          = 2
-  *            PLL_Q                          = 4
-  *            PLL_R                          = 2
-  *            VDD(V)                         = 3.3
-  *            Flash Latency(WS)              = 4
-  * @param  None
-  * @retval None
-  */
-static void SystemClock_Config(void)
-{
+ * @brief  System Clock Configuration
+ *         The system Clock is configured as follow :
+ *            System Clock source            = PLL (HSE)
+ *            SYSCLK(Hz)                     = 400000000 (Cortex-M7 CPU Clock)
+ *            HCLK(Hz)                       = 200000000 (Cortex-M4 CPU, Bus
+ * matrix Clocks) AHB Prescaler                  = 2 D1 APB3 Prescaler = 2 (APB3
+ * Clock  100MHz) D2 APB1 Prescaler              = 2 (APB1 Clock  100MHz) D2
+ * APB2 Prescaler              = 2 (APB2 Clock  100MHz) D3 APB4 Prescaler = 2
+ * (APB4 Clock  100MHz) HSE Frequency(Hz)              = 25000000 PLL_M = 5
+ *            PLL_N                          = 160
+ *            PLL_P                          = 2
+ *            PLL_Q                          = 4
+ *            PLL_R                          = 2
+ *            VDD(V)                         = 3.3
+ *            Flash Latency(WS)              = 4
+ * @param  None
+ * @retval None
+ */
+static void SystemClock_Config(void) {
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_OscInitTypeDef RCC_OscInitStruct;
   HAL_StatusTypeDef ret = HAL_OK;
@@ -110,12 +118,13 @@ static void SystemClock_Config(void)
   /*!< Supply configuration update enable */
   HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY);
 
-  /* The voltage scaling allows optimizing the power consumption when the device is
-     clocked below the maximum system frequency, to update the voltage scaling value
-     regarding system frequency refer to product datasheet.  */
+  /* The voltage scaling allows optimizing the power consumption when the device
+     is clocked below the maximum system frequency, to update the voltage
+     scaling value regarding system frequency refer to product datasheet.  */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+  }
 
   /* Enable HSE Oscillator and activate PLL with HSE as source */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
@@ -135,14 +144,14 @@ static void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
   RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
   ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
-  if(ret != HAL_OK)
-  {
+  if (ret != HAL_OK) {
     Error_Handler();
   }
 
-/* Select PLL as system clock source and configure  bus clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | \
-                                 RCC_CLOCKTYPE_PCLK2  | RCC_CLOCKTYPE_D3PCLK1);
+  /* Select PLL as system clock source and configure  bus clocks dividers */
+  RCC_ClkInitStruct.ClockType =
+      (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_D1PCLK1 |
+       RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1);
 
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
@@ -152,37 +161,37 @@ static void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
   ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
-  if(ret != HAL_OK)
-  {
+  if (ret != HAL_OK) {
     Error_Handler();
   }
 
- /*
-  Note : The activation of the I/O Compensation Cell is recommended with communication  interfaces
-          (GPIO, SPI, FMC, QSPI ...)  when  operating at  high frequencies(please refer to product datasheet)
-          The I/O Compensation Cell activation  procedure requires :
-        - The activation of the CSI clock
-        - The activation of the SYSCFG clock
-        - Enabling the I/O Compensation Cell : setting bit[0] of register SYSCFG_CCCSR
- */
+  /*
+   Note : The activation of the I/O Compensation Cell is recommended with
+   communication  interfaces (GPIO, SPI, FMC, QSPI ...)  when  operating at high
+   frequencies(please refer to product datasheet) The I/O Compensation Cell
+   activation  procedure requires :
+         - The activation of the CSI clock
+         - The activation of the SYSCFG clock
+         - Enabling the I/O Compensation Cell : setting bit[0] of register
+   SYSCFG_CCCSR
+  */
 
   /*activate CSI clock mondatory for I/O Compensation Cell*/
-  __HAL_RCC_CSI_ENABLE() ;
+  __HAL_RCC_CSI_ENABLE();
 
   /* Enable SYSCFG clock mondatory for I/O Compensation Cell */
-  __HAL_RCC_SYSCFG_CLK_ENABLE() ;
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
 
   /* Enables the I/O Compensation Cell */
   HAL_EnableCompensationCell();
 }
 
 /**
-  * @brief  Display main demo messages
-  * @param  None
-  * @retval None
-  */
-static void Display_DemoDescription(void)
-{
+ * @brief  Display main demo messages
+ * @param  None
+ * @retval None
+ */
+static void Display_DemoDescription(void) {
   char desc[64];
   uint32_t x_size;
   uint32_t y_size;
@@ -203,88 +212,79 @@ static void Display_DemoDescription(void)
   UTIL_LCD_DisplayStringAt(0, 10, (uint8_t *)"STM32H747I BSP", CENTER_MODE);
   UTIL_LCD_DisplayStringAt(0, 35, (uint8_t *)"Drivers examples", CENTER_MODE);
 
-
   UTIL_LCD_SetFont(&Font12);
-  UTIL_LCD_DisplayStringAt(0, y_size - 20, (uint8_t *)"Copyright (c) STMicroelectronics 2018", CENTER_MODE);
+  UTIL_LCD_DisplayStringAt(0, y_size - 20,
+                           (uint8_t *)"Copyright (c) STMicroelectronics 2018",
+                           CENTER_MODE);
 
   UTIL_LCD_SetFont(&Font16);
-  BSP_LCD_FillRect(0, 0, y_size/2 + 15, x_size, 60, UTIL_LCD_COLOR_BLUE);
+  BSP_LCD_FillRect(0, 0, y_size / 2 + 15, x_size, 60, UTIL_LCD_COLOR_BLUE);
   UTIL_LCD_SetTextColor(UTIL_LCD_COLOR_WHITE);
   UTIL_LCD_SetBackColor(UTIL_LCD_COLOR_BLUE);
-  UTIL_LCD_DisplayStringAt(0, y_size / 2 + 30, (uint8_t *)"Press Wakeup button to start :", CENTER_MODE);
-  sprintf(desc,"%s example", BSP_examples[DemoIndex].DemoName);
-  UTIL_LCD_DisplayStringAt(0, y_size/2 + 45, (uint8_t *)desc, CENTER_MODE);
+  UTIL_LCD_DisplayStringAt(
+      0, y_size / 2 + 30,
+      (uint8_t *)"Press Wakeup button to start :", CENTER_MODE);
 }
 
 /**
-  * @brief  Check for user input
-  * @param  None
-  * @retval Input state (1 : active / 0 : Inactive)
-  */
-uint8_t CheckForUserInput(void)
-{
-  return ButtonState;
-}
+ * @brief  Check for user input
+ * @param  None
+ * @retval Input state (1 : active / 0 : Inactive)
+ */
+uint8_t CheckForUserInput(void) { return ButtonState; }
 
 /**
-  * @brief  Button Callback
-  * @param  Button Specifies the pin connected EXTI line
-  * @retval None
-  */
-void BSP_PB_Callback(Button_TypeDef Button)
-{
-  if(Button == BUTTON_WAKEUP)
-  {
-
+ * @brief  Button Callback
+ * @param  Button Specifies the pin connected EXTI line
+ * @retval None
+ */
+void BSP_PB_Callback(Button_TypeDef Button) {
+  BSP_LED_Toggle(LED_GREEN);
+  if (Button == BUTTON_WAKEUP) {
     ButtonState = 1;
   }
-
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @param  None
+ * @retval None
+ */
+void Error_Handler(void) {
   /* Turn LED REDon */
   BSP_LED_On(LED_RED);
-  while(1)
-  {
+  while (1) {
   }
 }
 
 #ifdef USE_FULL_ASSERT
 
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{
-  /* User can add his own implementation to report the file name and line number,
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* User can add his own implementation to report the file name and line
+     number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
   /* Infinite loop */
-  while (1)
-  {
+  while (1) {
   }
 }
 #endif /* USE_FULL_ASSERT */
 
 /**
-  * @brief  Configure the MPU attributes as Write Through for SDRAM.
-  * @note   The Base Address is SDRAM_DEVICE_ADDR.
-  *         The Region Size is 32MB.
-  * @param  None
-  * @retval None
-  */
-static void MPU_Config(void)
-{
+ * @brief  Configure the MPU attributes as Write Through for SDRAM.
+ * @note   The Base Address is SDRAM_DEVICE_ADDR.
+ *         The Region Size is 32MB.
+ * @param  None
+ * @retval None
+ */
+static void MPU_Config(void) {
   MPU_Region_InitTypeDef MPU_InitStruct;
 
   /* Disable the MPU */
@@ -310,15 +310,75 @@ static void MPU_Config(void)
 }
 
 /**
-  * @brief  CPU L1-Cache enable.
-  * @param  None
-  * @retval None
-  */
-static void CPU_CACHE_Enable(void)
-{
+ * @brief  CPU L1-Cache enable.
+ * @param  None
+ * @retval None
+ */
+static void CPU_CACHE_Enable(void) {
   /* Enable I-Cache */
   SCB_EnableICache();
 
   /* Enable D-Cache */
   SCB_EnableDCache();
+}
+
+/**
+ * @brief  Camera Frame Event callback.
+ */
+void BSP_CAMERA_FrameEventCallback(uint32_t Instance) {
+  BSP_LED_On(LED_RED);
+  BSP_CAMERA_Suspend(0);
+
+  /* Convert captured frame to ARGB8888 and copy it to LCD FRAME BUFFER */
+  LCD_LL_Convert_RGB565ToARGB8888(
+      (uint32_t *)(CAMERA_FRAME_BUFFER), (uint32_t *)(LCD_LAYER_0_ADDRESS),
+      CameraResX[index_resolution], CameraResY[index_resolution]);
+
+  BSP_CAMERA_Resume(0);
+  BSP_LED_Off(LED_RED);
+}
+
+/**
+ * @brief  Copy the Captured Picture to the display Frame buffer.
+ * @param  pSrc: Pointer to source buffer
+ * @param  pDst: Pointer to destination buffer
+ * @retval None
+ */
+static void LCD_LL_Convert_RGB565ToARGB8888(void *pSrc, void *pDst,
+                                            uint16_t xsize, uint16_t ysize) {
+  uint32_t xPos, yPos, destination;
+  uint32_t LcdResX, LcdResY;
+  BSP_LCD_GetXSize(0, &LcdResX);
+  BSP_LCD_GetYSize(0, &LcdResY);
+  /* Configure the DMA2D Mode, Color Mode and output offset */
+  hlcd_dma2d.Init.Mode = DMA2D_M2M_PFC;
+  hlcd_dma2d.Init.ColorMode = DMA2D_OUTPUT_ARGB8888;
+  hlcd_dma2d.Init.OutputOffset = LcdResX - xsize;
+  /* DMA2D Callbacks Configuration */
+  hlcd_dma2d.XferCpltCallback = NULL;
+
+  /* Foreground Configuration */
+  hlcd_dma2d.LayerCfg[1].AlphaMode = DMA2D_REPLACE_ALPHA;
+  hlcd_dma2d.LayerCfg[1].InputAlpha = 0xFF;
+  hlcd_dma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_RGB565;
+  hlcd_dma2d.LayerCfg[1].InputOffset = 0;
+
+  hlcd_dma2d.Instance = DMA2D;
+
+  /* Calculate the destination transfer address */
+  xPos = (LcdResX - xsize) / 2;
+  yPos = (LcdResY - ysize) / 2;
+
+  destination = (uint32_t)pDst + (yPos * LcdResX + xPos) * 4;
+
+  /* DMA2D Initialization */
+  if (HAL_DMA2D_Init(&hlcd_dma2d) == HAL_OK) {
+    if (HAL_DMA2D_ConfigLayer(&hlcd_dma2d, 1) == HAL_OK) {
+      if (HAL_DMA2D_Start(&hlcd_dma2d, (uint32_t)pSrc, destination, xsize,
+                          ysize) == HAL_OK) {
+        /* Polling For DMA transfer */
+        HAL_DMA2D_PollForTransfer(&hlcd_dma2d, 100);
+      }
+    }
+  }
 }
